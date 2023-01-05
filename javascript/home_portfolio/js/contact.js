d7287b2f8070a7f0d8e2fe69b63c204a8390e9f2
jampanaharshitha_ldp <118795949+jampanaharshitha@users.noreply.github.com>
2023-01-05 17:50:27 +0530

    Add files via upload

---
+++ javascript/home_portfolio/js/contact.js
@@ -0,0 +1,9 @@
+function getdetails()
+        {
+var a = document.forms["details"]["firstname"].value;
+var b = document.forms["details"]["lastname"].value;
+var c= document.forms["details"]["email"].value;
+
+alert("Your name is "+a+" "+b+". Your e-mail is "+c+ "I will contact you for your queries");
+
+        }
